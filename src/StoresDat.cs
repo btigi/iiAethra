088a1d421a6abf6b37dcdcf229d43ea2953bb66e
btigi <btigi@users.noreply.github.com>
2025-04-12 12:50:55 +0100

    Add reading STORES.DAT

---
+++ src/StoresDat.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+namespace iiAethra
+{
+    public class StoresDat
+    {
+        public List<StoreRecord> Read(string filename)
+        {
+            const int MaxStoreNameLength = 40;
+            const int MaxItemNameLength = 22;
+
+            const int ShopSize = 2541;
+
+            var result = new List<StoreRecord>();
+            using var fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            using var br = new BinaryReader(fs);            
+            while (fs.Position < fs.Length && fs.Position + ShopSize < fs.Length)
+            {
+                var record = new StoreRecord();
+                var nameLength = br.ReadByte();
+                var name = br.ReadBytes(MaxStoreNameLength);
+                record.Name = Encoding.UTF8.GetString(name.Take(nameLength).ToArray());
+
+                for (int i = 0; i < 50; i++)
+                {
+                    var item = new ItemRecord();
+                    var itemNameLength = br.ReadByte();
+                    item.Name = Encoding.ASCII.GetString(br.ReadBytes(MaxItemNameLength).Take(itemNameLength).ToArray());
+                    item.Unknown = br.ReadBytes(18);
+                    record.Items.Add(item);
+                }
+
+                for (int i = 0; i < 50; i++)
+                {
+                    var item = record.Items.ElementAt(i);
+                    var cost = br.ReadBytes(6);
+                    item.Cost = Utils.Real48Convert(cost);
+                }
+
+                for (int i = 0; i < 50; i++)
+                {
+                    var item = record.Items.ElementAt(i);
+                    item.Id = br.ReadInt16();
+                }
+
+                for (int i = 0; i < 50; i++)
+                {
+                    var item = record.Items.ElementAt(i);
+                    item.Count = br.ReadByte ();
+                }
+
+                result.Add(record);
+            }
+            return result;
+        }
+    }
+
+    public class StoreRecord
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<ItemRecord> Items { get; set; } = new List<ItemRecord>();
+    }
+
+    public class ItemRecord
+    {
+        public string Name { get; set; } = string.Empty;
+        public byte[] Unknown { get; set; } = new byte[18];
+        public double Cost { get; set; }
+        public Int16 Id { get; set; }
+        public Int16 Count { get; set; }
+    }
+
+}
---
+++ src/Utils.cs
@@ -0,0 +1,45 @@
+﻿namespace iiAethra
+{
+    public static class Utils
+    {
+        public static double Real48Convert(byte[] real48)
+        {
+            // real48[0] == 0 is represents the value 0.
+            if (real48[0] == 0)
+                return 0.0;
+
+            Double exponentbase = 129d;
+            Double exponent = real48[0] - exponentbase; // The exponent is offset so deduct the base.
+
+            // Now Calculate the mantissa
+            Double mantissa = 0.0;
+            Double value = 1.0;
+            // For Each Byte.
+            for (int i = 5; i >= 1; i--)
+            {
+                int startbit = 7;
+                if (i == 5)
+                { startbit = 6; } //skip the sign bit.
+
+                //For Each Bit
+                for (int j = startbit; j >= 0; j--)
+                {
+                    value = value / 2;// Each bit is worth half the next bit but we're going backwards.
+                    if (((real48[i] >> j) & 1) == 1) //if this bit is set.
+                    {
+                        mantissa += value; // add the value.
+                    }
+                }
+            }
+
+            // The significand ia 1 + mantissa.
+            // This must come before applying the sign.
+            mantissa = 1 + mantissa;
+
+            if ((real48[5] & 0x80) != 0) // Sign bit check
+                mantissa = -mantissa;
+
+            return (mantissa) * Math.Pow(2.0, exponent);
+        }
+    }
+}
