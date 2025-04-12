08cf16e16c144d23e4cf65718162875212adbad6
btigi <btigi@users.noreply.github.com>
2025-04-12 09:52:12 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,27 @@
+﻿iiAscendancyLib
+=========
+
+iiAethra is a C# library supporting the modification of files relating to The Aethra Chronicles, the 1994 CRPG game.
+
+## Download
+
+Compiled downloads are not available.
+
+## Compiling
+
+To clone and run this application, you'll need [Git](https://git-scm.com) and [.NET](https://dotnet.microsoft.com/) installed on your computer. From your command line:
+
+```
+# Clone this repository
+$ git clone https://github.com/btigi/iiAethra
+
+# Go into the repository
+$ cd src
+
+# Build  the app
+$ dotnet build
+```
+
+## Licencing
+
+iiAethra is licenced under the MIT License. Full licence details are available in licence.md
---
+++ src/InfoDat.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace iiAethra
+{
+    public class InfoDat
+    {
+        public List<InfoDatRecord> Read(string filename)
+        {
+            const int MaxTextLength = 38;
+
+            var result = new List<InfoDatRecord>();
+            using var fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            using var br = new BinaryReader(fs);
+            while (fs.Position < fs.Length)
+            {
+                var record = new InfoDatRecord();
+                var length = br.BaseStream.ReadByte();
+                var bytes = br.ReadBytes(MaxTextLength);
+                var text = Encoding.UTF8.GetString(bytes.Take(length).ToArray());
+
+                var lineCount = br.ReadByte();
+                record.Lines = new List<string>(lineCount);
+                for (int i = 0; i < lineCount; i++)
+                {
+                    var lineLength = br.BaseStream.ReadByte();
+                    var lineBytes = br.ReadBytes(MaxTextLength);
+                    var lineText = Encoding.UTF8.GetString(lineBytes.Take(lineLength).ToArray());
+                    record.Lines.Add(lineText);
+                }
+                record.Title = text;
+                result.Add(record);
+            }
+            return result;
+        }
+    }
+
+    public class InfoDatRecord
+    {
+        public string Title { get; set; } = string.Empty;
+        public List<string> Lines { get; set; } = new List<string>();
+    }
+}
