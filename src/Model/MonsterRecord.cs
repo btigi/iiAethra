99918419b23dbeb90738f85f7fa396bb8f88936d
btigi <btigi@users.noreply.github.com>
2026-08-15 09:11:39 +0100

    Update NMONSTER.DAT handling

--- src/Model/MonsterRecord.cs
+++ src/Model/MonsterRecord.cs
@@ -3,20 +3,26 @@
     public class MonsterRecord
     {
         public string Name { get; set; } = string.Empty;
+        public byte Unknown { get; set; }
         public byte CombatIconId { get; set; }
-        public Int32 SpecialAbilities { get; set; }
-        public Int32 OffensiveBonus { get; set; }
-        public Int32 DefensiveBonus { get; set; }
+        public byte Unknown2 { get; set; }
+        public Int16 SpecialAbilities { get; set; }
+        public Int16 OffensiveBonus { get; set; }
+        public byte Unknown3 { get; set; }
+        public Int16 DefensiveBonus { get; set; }
+        public Int16 Unknown4 { get; set; }
         public byte MaxDamage { get; set; }
         public byte Swings { get; set; }
-        public Int32 MaxHits { get; set; }
-        public Int32 MaxHitsInfo { get; set; }
+        public Int16 MaxHits { get; set; }
+        public Int16 MaxHitsRelated { get; set; }
+        public Int16 Unknown5 { get; set; }
         public byte CastSpells { get; set; }
-        public byte SpellInfo { get; set; }
-        public Int32 MaxSpellInfo { get; set; }
+        public byte SpellList { get; set; }
+        public Int16 MaxSpellPoints { get; set; }
+        public byte Unknown6 { get; set; }
         public byte MonsterType { get; set; }
-        public Int32 MagicResistance { get; set; }
+        public Int16 MagicResistance { get; set; }
+        public byte[] Unknown7 { get; set; } = new byte[8];
         public byte CombatIconSize { get; set; }
-        public byte PortraitId { get; set; }
     }
 }
--- src/NMonsterDat.cs
+++ src/NMonsterDat.cs
@@ -5,32 +5,42 @@ namespace ii.Aethra
 {
     public class NMonsterDat
     {
+        public const int MaxNameLength = 20;
+        public const int RecordSizeBytes = 58;
+        public const int RecordCount = 130;
+
         public List<MonsterRecord> Read(string filename)
         {
             var result = new List<MonsterRecord>();
             using var fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
             using var br = new BinaryReader(fs);
-            while (fs.Position < fs.Length)
+            while (fs.Position + RecordSizeBytes <= fs.Length)
             {
                 var record = new MonsterRecord();
                 var nameLength = br.ReadByte();
-                var name = br.ReadBytes(21);
+                var name = br.ReadBytes(MaxNameLength);
                 record.Name = Encoding.UTF8.GetString(name.Take(nameLength).ToArray());
+                record.Unknown = br.ReadByte();
                 record.CombatIconId = br.ReadByte();
-                record.SpecialAbilities = br.ReadInt32();
-                record.OffensiveBonus = br.ReadInt32();
-                record.DefensiveBonus = br.ReadInt32();
+                record.Unknown2 = br.ReadByte();
+                record.SpecialAbilities = br.ReadInt16();
+                record.OffensiveBonus = br.ReadInt16();
+                record.Unknown3 = br.ReadByte();
+                record.DefensiveBonus = br.ReadInt16();
+                record.Unknown4 = br.ReadInt16();
                 record.MaxDamage = br.ReadByte();
                 record.Swings = br.ReadByte();
-                record.MaxHits = br.ReadInt32();
-                record.MaxHitsInfo = br.ReadInt32();
+                record.MaxHits = br.ReadInt16();
+                record.MaxHitsRelated = br.ReadInt16();
+                record.Unknown5 = br.ReadInt16();
                 record.CastSpells = br.ReadByte();
-                record.SpellInfo = br.ReadByte();
-                record.MaxSpellInfo = br.ReadInt32();
+                record.SpellList = br.ReadByte();
+                record.MaxSpellPoints = br.ReadInt16();
+                record.Unknown6 = br.ReadByte();
                 record.MonsterType = br.ReadByte();
-                record.MagicResistance = br.ReadInt32();
+                record.MagicResistance = br.ReadInt16();
+                record.Unknown7 = br.ReadBytes(8);
                 record.CombatIconSize = br.ReadByte();
-                record.PortraitId = br.ReadByte();
                 result.Add(record);
             }
             return result;
@@ -43,25 +53,31 @@ namespace ii.Aethra
             foreach (var record in records)
             {
                 var nameBytes = Encoding.UTF8.GetBytes(record.Name);
-                var nameLength = (byte)Math.Min(nameBytes.Length, 21);
+                var nameLength = (byte)Math.Min(nameBytes.Length, MaxNameLength);
                 bw.Write(nameLength);
                 bw.Write(nameBytes.Take(nameLength).ToArray());
-                bw.Write(new byte[21 - nameLength]);
+                bw.Write(new byte[MaxNameLength - nameLength]);
+                bw.Write(record.Unknown);
                 bw.Write(record.CombatIconId);
+                bw.Write(record.Unknown2);
                 bw.Write(record.SpecialAbilities);
                 bw.Write(record.OffensiveBonus);
+                bw.Write(record.Unknown3);
                 bw.Write(record.DefensiveBonus);
+                bw.Write(record.Unknown4);
                 bw.Write(record.MaxDamage);
                 bw.Write(record.Swings);
                 bw.Write(record.MaxHits);
-                bw.Write(record.MaxHitsInfo);
+                bw.Write(record.MaxHitsRelated);
+                bw.Write(record.Unknown5);
                 bw.Write(record.CastSpells);
-                bw.Write(record.SpellInfo);
-                bw.Write(record.MaxSpellInfo);
+                bw.Write(record.SpellList);
+                bw.Write(record.MaxSpellPoints);
+                bw.Write(record.Unknown6);
                 bw.Write(record.MonsterType);
                 bw.Write(record.MagicResistance);
+                bw.Write(record.Unknown7);
                 bw.Write(record.CombatIconSize);
-                bw.Write(record.PortraitId);
             }
         }
     }
